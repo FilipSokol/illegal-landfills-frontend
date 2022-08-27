2e02f89ed5d828bcd97f1aefc28557b765322b29
Filip Sokol <sok.filip@gmail.com>
2022-08-27 15:37:24 +0200

    Add markers backend

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,17 @@
-import { useState, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import {
   GoogleMap,
   useJsApiLoader,
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
+import Axios from "axios";
 
 import markerIcon from "../images/markerIcon.png";
 
 const containerStyle = {
-  width: "960px",
-  height: "500px",
+  width: "100%",
+  height: "100%",
 };
 
 const options = {
@@ -23,32 +24,17 @@ const center = {
   lng: 19.357910156250004,
 };
 
-const markers = [
-  {
-    id: "1",
-    name: "Test1",
-    position: {
-      lat: 50.42806899872329,
-      lng: 19.400525093078617,
-    },
-    image: "https://i.imgur.com/0gtMdoC.jpg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-  {
-    id: "2",
-    name: "Test2",
-    position: {
-      lat: 50.30042898028658,
-      lng: 18.710660934448246,
-    },
-    image: "https://i.imgur.com/4A4BXFI.jpg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-];
-
 function Map() {
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/markers").then((response) => {
+      setMarkers(response.data);
+    });
+  }, []);
+
+  console.log(markers);
+
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -73,7 +59,10 @@ function Map() {
   };
 
   return isLoaded ? (
-    <div className="w-100 h-auto p-20 flex justify-center">
+    //   <div className="h-hero bg-hero-img bg-right-bottom bg-cover flex">
+    //   <div rel="preload" className="h-hero w-full bg-white bg-opacity-50"></div>
+    // </div>
+    <div className="w-100 h-hero flex">
       <GoogleMap
         mapContainerStyle={containerStyle}
         options={options}
@@ -82,64 +71,74 @@ function Map() {
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
-        {markers.map(({ id, name, position, image, description }, i, arr) => {
-          return (
-            <Marker
-              key={id}
-              icon={markerIcon}
-              position={position}
-              onClick={() => handleActiveMarker(id)}
-            >
-              {activeMarker === id ? (
-                <InfoWindow
-                  className="w-20"
-                  onCloseClick={() => setActiveMarker(null)}
-                >
-                  <div className="h-100 w-100 flex justify-start font-montserrat">
-                    <div className="h-auto w-auto">
-                      <img
-                        src={image}
-                        alt={name}
-                        className="w-72 h-full object-cover rounded-lg"
-                      />
+        {markers.map(
+          (
+            { markerid, latitude, longitude, imageurl, description },
+            i,
+            arr
+          ) => {
+            const position = {
+              lat: latitude,
+              lng: longitude,
+            };
+            return (
+              <Marker
+                key={markerid}
+                icon={markerIcon}
+                position={position}
+                onClick={() => handleActiveMarker(markerid)}
+              >
+                {activeMarker === markerid ? (
+                  <InfoWindow
+                    className="w-20"
+                    onCloseClick={() => setActiveMarker(null)}
+                  >
+                    <div className="h-100 w-100 flex justify-start font-montserrat">
+                      <div className="h-auto w-auto">
+                        <img
+                          src={imageurl}
+                          alt="TEST" //! ZMIENIC
+                          className="w-72 h-full object-cover rounded-lg"
+                        />
+                      </div>
+                      <div className="flex flex-col pl-2 text-lightblack">
+                        <div className="h-auto w-auto flex flex-col">
+                          <div className="h-10 w-full text-2xl flex justify-center items-center rounded-t-lg  bg-lightgreen text-white">
+                            Opis
+                          </div>
+                          <div className="h-24 w-full p-2 shadow-md flex justify-start items-start rounded-b-lg">
+                            {description}
+                          </div>
+                        </div>
+                        <div className="h-auto w-auto flex flex-col mt-3">
+                          <div className="h-10 w-full text-2xl flex justify-center items-center rounded-t-lg  bg-lightgreen text-white">
+                            Aktualizacje
+                          </div>
+                          <div className="h-12 w-full p-2 shadow-md flex justify-start items-start rounded-b-lg">
+                            19/05/2022 - Posprzątane
+                            <br />
+                            Zgłoszenie zostanie zarchiwizowane za 2 dni
+                          </div>
+                        </div>
+                        <div className="flex flex-col h-auto w-full mt-3 justify-center items-start">
+                          <button className="h-10 w-full bg-sky-400	 rounded-lg text-white text-lg shadow-md">
+                            Posprzątane
+                          </button>
+                          <button className="mt-3 h-10 w-full bg-yellow-400 rounded-lg text-white text-lg shadow-md">
+                            Nadal tutaj jest
+                          </button>
+                          <button className="mt-3 h-10 w-full bg-red-500 rounded-lg text-white text-lg shadow-md">
+                            Zgłoś spam
+                          </button>
+                        </div>
+                      </div>
                     </div>
-                    <div className="flex flex-col pl-2 text-lightblack">
-                      <div className="h-auto w-auto flex flex-col">
-                        <div className="h-10 w-full text-2xl flex justify-center items-center rounded-t-lg  bg-lightgreen text-white">
-                          Opis
-                        </div>
-                        <div className="h-24 w-full p-2 shadow-md flex justify-start items-start rounded-b-lg">
-                          {description}
-                        </div>
-                      </div>
-                      <div className="h-auto w-auto flex flex-col mt-3">
-                        <div className="h-10 w-full text-2xl flex justify-center items-center rounded-t-lg  bg-lightgreen text-white">
-                          Aktualizacje
-                        </div>
-                        <div className="h-12 w-full p-2 shadow-md flex justify-start items-start rounded-b-lg">
-                          19/05/2022 - Posprzątane
-                          <br />
-                          Zgłoszenie zostanie zarchiwizowane za 2 dni
-                        </div>
-                      </div>
-                      <div className="flex flex-col h-auto w-full mt-3 justify-center items-start">
-                        <button className="h-10 w-full bg-sky-400	 rounded-lg text-white text-lg shadow-md">
-                          Posprzątane
-                        </button>
-                        <button className="mt-3 h-10 w-full bg-yellow-400 rounded-lg text-white text-lg shadow-md">
-                          Nadal tutaj jest
-                        </button>
-                        <button className="mt-3 h-10 w-full bg-red-500 rounded-lg text-white text-lg shadow-md">
-                          Zgłoś spam
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </InfoWindow>
-              ) : null}
-            </Marker>
-          );
-        })}
+                  </InfoWindow>
+                ) : null}
+              </Marker>
+            );
+          }
+        )}
       </GoogleMap>
     </div>
   ) : (
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,9 @@
 //  import React, { useEffect, useRef } from "react";
-import Hero from "../components/Hero";
 import Map from "../components/Map";
 
 function Home() {
   return (
     <div className="m-0">
-      <Hero />
       <Map />
     </div>
   );
